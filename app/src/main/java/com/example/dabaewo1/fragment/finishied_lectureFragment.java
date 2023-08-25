f2de8ba5160930144e6cd4ce5adbe8343e8c2df8
kiyeong <rldud1237@naver.com>
2023-08-26 07:39:22 +0900

    나의 강좌 2차

--- app/src/main/java/com/example/dabaewo1/fragment/finishied_lectureFragment.java
+++ app/src/main/java/com/example/dabaewo1/fragment/finishied_lectureFragment.java
@@ -10,19 +10,11 @@ import android.view.ViewGroup;
 
 import com.example.dabaewo1.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link finishied_lectureFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class finishied_lectureFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
